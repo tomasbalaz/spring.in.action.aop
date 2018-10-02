cab200e7757afec3236697199b7423ad3bd7d223
tomas.balaz <tomas.balaz@asseco-ce.com>
2018-10-02 08:55:53 +0200

    Update Audience AOP execution packages

--- src/main/java/spring/in/action/aop/aspect/Audience.java
+++ src/main/java/spring/in/action/aop/aspect/Audience.java
@@ -8,23 +8,23 @@ import org.aspectj.lang.annotation.Before;
 @Aspect
 public class Audience {
 
-	@Before("execution(** concert.Performance.perform(..))")
+	@Before("execution(** spring.in.action.aop.model.Performance.perform(..))")
 	public void silenceCellPhones() {
 
 		System.out.println("Silencing cell phones");
 	}
 
-	@Before("execution(** concert.Performance.perform(..))")
+	@Before("execution(** spring.in.action.aop.model.Performance.perform(..))")
 	public void takeSeats() {
 		System.out.println("Taking seats");
 	}
 
-	@AfterReturning("execution(** concert.Performance.perform(..))")
+	@AfterReturning("execution(** spring.in.action.aop.model.Performance.perform(..))")
 	public void applause() {
 		System.out.println("CLAP CLAP CLAP!!!");
 	}
 
-	@AfterThrowing("execution(** concert.Performance.perform(..))")
+	@AfterThrowing("execution(** spring.in.action.aop.model.Performance.perform(..))")
 	public void demandRefund() {
 		System.out.println("Demanding a refund");
 	}
