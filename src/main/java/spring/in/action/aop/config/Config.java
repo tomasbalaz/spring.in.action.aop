f3a09d60daf1c915adbc67d60f6317292deb8454
tomas.balaz <tomas.balaz@asseco-ce.com>
2018-10-02 14:44:52 +0200

    Enable Aspect Audience in Config class

--- src/main/java/spring/in/action/aop/config/Config.java
+++ src/main/java/spring/in/action/aop/config/Config.java
@@ -1,12 +1,19 @@
 package spring.in.action.aop.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import spring.in.action.aop.aspect.Audience;
 
 @Configuration
 @EnableAspectJAutoProxy
 @ComponentScan(basePackages= {"spring.in.action.aop"})
 public class Config {
-
+	
+	@Bean
+	public Audience audience() {
+	return new Audience();
+	}
 }
