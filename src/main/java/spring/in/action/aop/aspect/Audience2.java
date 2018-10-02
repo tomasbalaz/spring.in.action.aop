10ec3066c615a867726242bf47104fbb398e55c9
tomas.balaz <tomas.balaz@asseco-ce.com>
2018-10-02 14:49:15 +0200

    Create Audience2 Aspect class

---
+++ src/main/java/spring/in/action/aop/aspect/Audience2.java
@@ -0,0 +1,36 @@
+package spring.in.action.aop.aspect;
+
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class Audience2 {
+	
+	@Pointcut("execution(** concert.Performance.perform(..))")
+	public void performance() {}
+
+	@Before("performance()")
+	public void silenceCellPhones() {
+
+		System.out.println("Silencing cell phones");
+	}
+
+	@Before("performance()")
+	public void takeSeats() {
+		System.out.println("Taking seats");
+	}
+
+	@AfterReturning("performance()")
+	public void applause() {
+		System.out.println("CLAP CLAP CLAP!!!");
+	}
+
+	@AfterThrowing("performance()")
+	public void demandRefund() {
+		System.out.println("Demanding a refund");
+	}
+
+}
