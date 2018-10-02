34527b3386d4cddd6073d8a1000041f375836420
tomas.balaz <tomas.balaz@asseco-ce.com>
2018-10-02 08:42:11 +0200

    Update config file

--- src/main/java/spring/in/action/aop/config/Config.java
+++ src/main/java/spring/in/action/aop/config/Config.java
@@ -1,8 +1,12 @@
 package spring.in.action.aop.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
+@EnableAspectJAutoProxy
+@ComponentScan(basePackages= {"spring.in.action.aop"})
 public class Config {
 
 }
